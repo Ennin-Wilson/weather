0e63f2acfa91285073906934671128c119dec6ae
Vert Ennin Wilson <87149619+Vert-Nk26@users.noreply.github.com>
2023-09-20 15:17:00 +0200

    weather_app

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather App
+
+This is a simple Python program that allows users to retrieve and display current weather information for a specified city or location. It uses the OpenWeatherMap API to fetch weather data and provides details such as temperature, humidity, description, and wind speed.
+
+## Features
+
+- Fetches current weather data based on user input.
+- Displays temperature in Celsius.
+- Provides information on humidity, weather description, and wind speed.
+- Handles errors gracefully, such as incorrect user input or API request issues.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x installed on your machine.
+- Internet connection to make API requests.
+- OpenWeatherMap API key. (You can obtain one by signing up at https://openweathermap.org/api)
+
---
+++ weather.py
@@ -0,0 +1,47 @@
+import requests
+import json
+from sys import exit
+from math import ceil
+
+# my api key
+api_key = "5b282d97650483f39582273240604b4c"
+
+
+def main():
+    return weather_app()
+
+
+def weather_app():
+    user_input = input("Enter city or location: ")
+    store_details = []
+    if user_input.isdigit():
+        exit("MUST BE A CITY OR LOCATION NOT A NUMBER!")
+    else:
+        try:
+            respons = requests.get(
+                f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}&units=metric"
+            )
+            if respons.status_code == 200:
+                json_data = respons.json()
+                if "weather" in json_data and json_data["weather"]:
+                    weather_description = json_data["weather"][0]["description"]
+                    temperature = json_data["main"]["temp"]
+                    humidity = json_data["main"]["humidity"]
+                    wind_speed = json_data["wind"]["speed"]
+                    location_name = json_data["name"]
+
+                    print(f"Weather in {location_name}")
+                    print(f"Temperature: {temperature:.2f}˚C")
+                    print(f"Humidity: {humidity}%")
+                    print(f"Description: {weather_description}")
+                    print(f"Wind Speed: {wind_speed} Km/h")
+                else:
+                    print("Weather data is incomplete. Please try again later.")
+            else:
+                print("City or Location not found. Please check your input.")
+
+        except requests.RequestException as e:
+            exit(f"An error occurred: {e}")
+
+
+print(main())
